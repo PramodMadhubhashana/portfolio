fece88699819aaf16511caabf674c7c56bbd6865
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-06-23 19:50:01 +0530

    add the project

--- ProjectData/ProjeectData.jsx
+++ ProjectData/ProjeectData.jsx
@@ -7,4 +7,12 @@ export const projectData = [
         link: '/',
         github: '/'
     },
+    {
+        image: '/assets/work/1717395285827.jpeg',
+        category: 'web',
+        name: 'NextStyle',
+        description: 'Developed a comprehensive e-commerce management system utilizing MySQL and MongoDB for efficient order, customer, and product management. Integrated customer reviews and feedback, showcasing full-stack skills with PHP, HTML, and CSS. This project highlights my expertise in database management, web development, and seamless technology integration.',
+        link: '/',
+        github: '/'
+    },
 ];
