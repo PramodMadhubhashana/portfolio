26180cfd3f0db5a9c2c38d23a03e0d304920faa3
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-06-23 19:36:52 +0530

    check

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,39 +16,39 @@ const Projects = () => {
   const filteredProject = projectData.filter((project) => {
     return category === 'all project' ? project : project.category === category;
   });
-  return (
-    <div className="container pt-12 max-w-[1100px] flex flex-col">
-      <div>
-        <p className="section-title bg-gradient-to-r from-blue-600 to-[#07D2FB] bg-clip-text text-transparent align-middle justify-center ">
-          My Projects
-        </p>
-      </div>
-      <div className="pt-10 w-full m-0">
-        <Tabs defaultValue={category}>
-          <TabsList className="border flex flex-wrap justify-center gap-x-3 w-auto">
-            {categories.map((category, index) => {
-              return (
-                <TabsTrigger
-                  onClick={() => setCategory(category)}
-                  value={category}
-                  key={index}
-                  className="capitalize">{category}</TabsTrigger>
-              );
-            })}
-          </TabsList>
-          <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-2 pt-5 gap-4">
-            {filteredProject.map((project, index) => {
-              return (
-                <TabsContent value={category} key={index}>
-                  <ProjectCard project={project} />
-                </TabsContent>
-              );
-            })}
-          </div>
-        </Tabs>
-      </div>
+  return ( <div>pramod</div>
+    // <div className="container pt-12 max-w-[1100px] flex flex-col">
+    //   <div>
+    //     <p className="section-title bg-gradient-to-r from-blue-600 to-[#07D2FB] bg-clip-text text-transparent align-middle justify-center ">
+    //       My Projects
+    //     </p>
+    //   </div>
+    //   <div className="pt-10 w-full m-0">
+    //     <Tabs defaultValue={category}>
+    //       <TabsList className="border flex flex-wrap justify-center gap-x-3 w-auto">
+    //         {categories.map((category, index) => {
+    //           return (
+    //             <TabsTrigger
+    //               onClick={() => setCategory(category)}
+    //               value={category}
+    //               key={index}
+    //               className="capitalize">{category}</TabsTrigger>
+    //           );
+    //         })}
+    //       </TabsList>
+    //       <div className="text-lg xl:mt-8 grid grid-cols-1 lg:grid-cols-2 pt-5 gap-4">
+    //         {filteredProject.map((project, index) => {
+    //           return (
+    //             <TabsContent value={category} key={index}>
+    //               <ProjectCard project={project} />
+    //             </TabsContent>
+    //           );
+    //         })}
+    //       </div>
+    //     </Tabs>
+    //   </div>
 
-    </div>
+    // </div>
   )
 }
 
