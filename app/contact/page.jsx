07a73ebf33e0f32fbacc3fae2b83b6a71fe7d90e
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-06-25 20:49:00 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -7,6 +7,7 @@ import {
   User,
   ArrowRightIcon,
   MessageSquare,
+  Layout,
 } from "lucide-react";
 import Image from "next/image";
 import React from "react";
@@ -51,68 +52,70 @@ const contact = () => {
     }
   };
   return (
-    <div className="container max-w-[1100px] justify-center items-center flex flex-col ">
-      <div className="flex flex-col justify-between items-center lg:flex-row w-full">
-        <div className="flex flex-col justify-center">
-          <div className="flex items-center gap-x-4 text-blue-500 text-lg mb-4">
-            <span className="w-[30px] h-[2px] bg-blue-500"></span>
-            <div>Say Hello 👋</div>
-          </div>
-          <div>
-            <div className="pt-10">
-              <h1 className="text-5xl font-bold max-w-md mb-8">Let's Work Together</h1>
+    <Layout>
+      <div className="container max-w-[1100px] justify-center items-center flex flex-col ">
+        <div className="flex flex-col justify-between items-center lg:flex-row w-full">
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-blue-500 text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-blue-500"></span>
+              <div>Say Hello 👋</div>
             </div>
             <div>
-              <p className="text-[20px] text-muted-foreground max-w-[480px]">
-                Connect with me via WhatsApp, LinkedIn, Facebook, Instagram, Twitter, and GitHub for inquiries and collaboration.
-              </p>
+              <div className="pt-10">
+                <h1 className="text-5xl font-bold max-w-md mb-8">Let's Work Together</h1>
+              </div>
+              <div>
+                <p className="text-[20px] text-muted-foreground max-w-[480px]">
+                  Connect with me via WhatsApp, LinkedIn, Facebook, Instagram, Twitter, and GitHub for inquiries and collaboration.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="pt-10">
+            <Image src='/assets/contact/ContactUs.png' width={500} height={500} alt="" priority />
+          </div>
+        </div>
+        <div className="w-full flex justify-center flex-col lg:flex-row lg:justify-between pt-20 sm:pt-5">
+          <div className="justify-center flex flex-col items-center">
+            <div className="text-[16px]">
+              {infoData.map((item, index) => {
+                return (
+                  <div className="flex items-center gap-x-4 mx-auto pb-5 text-[14px] sm:text-[16px]" key={index}>
+                    <div className="text-blue-600">{item.icon}</div>
+                    <div>{item.text}</div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex pt-5">
+              <Socials containerStyles='flex flex-row gap-x-5' iconsStyle='text-foreground text-[25px] sm:text-[30px] hover:text-primary transition-all' />
+            </div>
+          </div>
+          <div className="flex justify-center lg:justify-end w-full">
+            <div className="pt-5 lg:pt-0 w-[500px]">
+              <form className="flex flex-col gap-y-4" onSubmit={onSubmit}>
+                <div className="relative flex items-center">
+                  <Input type='text' name="name" placeholder='Name' required />
+                  <User className="absolute right-6" size={20} />
+                </div>
+                <div className="relative flex items-center">
+                  <Input type="email" name="email" placeholder='Email' required />
+                  <MailIcon className="absolute right-6" size={20} />
+                </div>
+                <div className="relative flex items-center">
+                  <Textarea name="message" placeholder='Type Your Message Here.' className='h-[180px]' required />
+                  <MessageSquare className="absolute right-6 top-4" size={20} />
+                </div>
+                <div className="flex justify-center">
+                  <Button type="submit" className='text-[16px] bg-gradient-to-r from-blue-500 to-[#07D2FB] text-white gap-x-2 max-w-[250px] w-[250px]'>Let's Talk <ArrowRightIcon size={18} /></Button>
+                </div>
+              </form>
+
             </div>
           </div>
         </div>
-        <div className="pt-10">
-          <Image src='/assets/contact/ContactUs.png' width={500} height={500} alt="" priority />
-        </div>
       </div>
-      <div className="w-full flex justify-center flex-col lg:flex-row lg:justify-between pt-20 sm:pt-5">
-        <div className="justify-center flex flex-col items-center">
-          <div className="text-[16px]">
-            {infoData.map((item, index) => {
-              return (
-                <div className="flex items-center gap-x-4 mx-auto pb-5 text-[14px] sm:text-[16px]" key={index}>
-                  <div className="text-blue-600">{item.icon}</div>
-                  <div>{item.text}</div>
-                </div>
-              );
-            })}
-          </div>
-          <div className="flex pt-5">
-            <Socials containerStyles='flex flex-row gap-x-5' iconsStyle='text-foreground text-[25px] sm:text-[30px] hover:text-primary transition-all' />
-          </div>
-        </div>
-        <div className="flex justify-center lg:justify-end w-full">
-          <div className="pt-5 lg:pt-0 w-[500px]">
-            <form className="flex flex-col gap-y-4" onSubmit={onSubmit}>
-              <div className="relative flex items-center">
-                <Input type='text' name="name" placeholder='Name' required />
-                <User className="absolute right-6" size={20} />
-              </div>
-              <div className="relative flex items-center">
-                <Input type="email" name="email" placeholder='Email' required />
-                <MailIcon className="absolute right-6" size={20} />
-              </div>
-              <div className="relative flex items-center">
-                <Textarea name="message" placeholder='Type Your Message Here.' className='h-[180px]' required />
-                <MessageSquare className="absolute right-6 top-4" size={20} />
-              </div>
-              <div className="flex justify-center">
-                <Button type="submit" className='text-[16px] bg-gradient-to-r from-blue-500 to-[#07D2FB] text-white gap-x-2 max-w-[250px] w-[250px]'>Let's Talk <ArrowRightIcon size={18} /></Button>
-              </div>
-            </form>
-
-          </div>
-        </div>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
