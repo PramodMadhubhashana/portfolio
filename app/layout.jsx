03e2f1b64b1d02fe4d1e763b3bd377be729b6632
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-07-01 16:32:43 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,8 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <Head>
         <title>{metadata.title}</title>
-        <link rel="icon" href="/assets/logo/LogoBlackpng.svg"/>
-        <link rel="icon" href="https://www.pramodmadhubhashana.com/assets/logo/LogoBlackpng.svg"/>
+        {/* <link rel="icon" href="/assets/logo/LogoBlackpng.svg"/> */}
+        <link rel="icon" href="https://www.pramodmadhubhashana.com/assets/logo/LogoBlackpng.png"/>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={metadata.description} />
